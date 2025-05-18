a60e899b571a7a74b3700f1567d372287939cade
xDEFCONx <mastersimtropolis@gmail.com>
2025-05-17 21:57:13 -0400

    launcher now looks in XboxGames directory

--- ProjectEconomyLauncher.bat
+++ ProjectEconomyLauncher.bat
@@ -9,11 +9,16 @@ call "Updater.bat"
 :: Locate MinecraftLauncher.exe
 
 for %%D in (C D E F G H I J K L M N O P Q R S T U V W X Y Z) do (
-    set "launcherPath=%%D:\Program Files (x86)\Minecraft Launcher\MinecraftLauncher.exe"
-    if exist "!launcherPath!" (
+    set "launcherPath1=%%D:\Program Files (x86)\Minecraft Launcher\MinecraftLauncher.exe"
+    set "launcherPath2=%%D:\XboxGames\Minecraft Launcher\Content\Minecraft.exe"
+    if exist "!launcherPath1!" (
+        start "" "!launcherPath1!"
+        goto :eof
+    )
+    if exist "!launcherPath2!" (
+        echo MinecraftLauncher.exe not found in Program Files(x86) on any drive.
         start "" "!launcherPath!"
         goto :eof
     )
+    echo Minecraft.exe not found in XboxGames on any drive.
 )
-
-echo MinecraftLauncher.exe not found in Program Files(x86) on any drive.
